d889c1cfe2b28b0912969e7cb2ac610d379a390e
asparadise33 <abby.paradise@gmail.com>
2024-06-19 09:16:05 -0500

    added a user function for user name specificity for the welcome page

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,12 +1,14 @@
 import renderToDom from '../utils/renderToDom';
 import clearDom from '../utils/clearDom';
 
-const homePage = () => {
+const userName = () => 'Name';
+
+const homePage = (user) => {
   clearDom();
   let homeString = '';
   homeString += `<div id="home-page" class="card">
         <div class="card-body">
-          <h1 class="card-title">Welcome, user-name!</h1></div>
+          <h1 class="card-title">Welcome, ${userName(user)} </h1></div>
           <div id="home-buttons"> <button type="button" id="view-order-btn" class="btn btn-success">View Orders</button>
           <button type="button" id="create-order-btn" class="btn btn-info">Create an Order</button>
           <button type="button" id="view-revenue-btn" class="btn btn-warning">View/Revenue</button>
@@ -15,4 +17,4 @@ const homePage = () => {
   renderToDom('#app', homeString);
 };
 
-export default homePage;
+export { homePage as default, userName };
