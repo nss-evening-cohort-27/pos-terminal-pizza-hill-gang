1adb478e2f7403809a483b0c6e20b07ee8f3d54d
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-06-18 21:32:08 -0400

    Debugged formEvents

--- events/formEvents.js
+++ events/formEvents.js
@@ -1,51 +1,5 @@
-// Will import fuction from api data
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { showOrders } from "../pages/orders";
-
-const formEvents = (user) => {
-    document.querySelector('#main-container').addEventListener('submit', (e) => {
-      e.preventDefault();
-      // TODO: CLICK EVENT FOR SUBMITTING Order Form
-      if (e.target.id.includes('submit-order')) {
-        const payload = {
-          orderName: document.querySelector('#order-name').value,
-          customerPhone: document.querySelector('#customer-phone').value,
-          customerEmail : document.querySelector('#customer-email').value,
-          uid: user.id
-        };
-
-        //  Will rename create, update, and get functions once data fuctions are available.
-
-        createOrders(payload).then(({ name }) => {
-          const patchPayload = { firebaseKey: name };
-          updateOrders(patchPayload).then(() => {
-            getFoodCards().then(showOrders);
-          });
-        });
-      }
-      // TODO: CLICK EVENT FOR EDITING Order Form
-      if (e.target.id.includes('update-order')) {
-        const [, firebaseKey] = e.target.id.split('--');
-        const payload = {
-          orderName: document.querySelector('#order-name').value,
-          customerPhone: document.querySelector('#customer-phone').value,
-          customerEmail : document.querySelector('#customer-email').value,
-          firebaseKey,
-        };
-        updateOrders(payload).then(() => {
-          getOders().then(showOrders);
-        });
-      }
-    });
-  };
-  
-  export default formEvents;
-  
-=======
-=======
->>>>>>> 7bd6df80adf5fa8cbc00a5226a09d6d7efe792e6
-import { showOrders } from '../../pages/orders';
+import { createOrders, updateOrder, getOrders } from '../api/orderData';
+import { showOrders } from '../pages/orders';
 
 const formEvents = (user) => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
@@ -61,7 +15,7 @@ const formEvents = (user) => {
 
       //  Will rename create, update, and get functions once data fuctions are available.
 
-      createOrder(payload).then(({ name }) => {
+      createOrders(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
 
         updateOrder(patchPayload).then(() => {
@@ -86,7 +40,3 @@ const formEvents = (user) => {
 };
 
 export default formEvents;
-<<<<<<< HEAD
->>>>>>> 7bd6df80adf5fa8cbc00a5226a09d6d7efe792e6
-=======
->>>>>>> 7bd6df80adf5fa8cbc00a5226a09d6d7efe792e6
