6276ab526321f3f5d64cf2a8d37d5667841cb800
asparadise33 <abby.paradise@gmail.com>
2024-06-21 16:52:16 -0500

    edit form is now appearing on the dom when edit button is clicked

--- components/forms/createItemsForm.js
+++ components/forms/createItemsForm.js
@@ -8,11 +8,11 @@ const createItemForm = (obj = {}) => {
     <form id="${obj.firebaseKey ? `update-item--${obj.firebaseKey}` : 'submit-item'}" class="mb-4">
       <div class="form-group">
         <label for="itemName">Item Name</label>
-        <input type="text" class="form-control" id="item-name" aria-describedby="itemName" placeholder="Enter Item name" value="${obj.items.name || ''}" required>
+        <input type="text" class="form-control" id="item-name" aria-describedby="itemName" placeholder="Enter Item name" value="${obj.name || ''}" required>
       </div>
       <div class="form-group">
         <label for="priceItem">item price</label>
-        <textarea class="form-control" placeholder="" id="item-price" style="height: 100px">${obj.items.price || ''}</textarea>
+        <textarea class="form-control" placeholder="" id="item-price" style="height: 100px">${obj.price || ''}</textarea>
       </div>
       <button id="submit-item-btn" type="submit" class="btn btn-primary">Add/Edit Item
       </button>
