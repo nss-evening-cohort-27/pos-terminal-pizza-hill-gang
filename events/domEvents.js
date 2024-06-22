400839aa9a04460ee195a9e61ca38838e6821ce6
Skeo <skeo051600@gmail.com>
2024-06-21 23:27:42 -0500

    Fix styling issues and form rendering issues

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,7 +1,7 @@
 import { deleteOrder, getOrders, getSingleOrder } from '../api/orderData';
 import createItemForm from '../components/forms/createItemsForm';
 import createOrderForm from '../components/forms/createOrderForm';
-import { emptyItems, showItems } from '../pages/items';
+// import { showItems } from '../pages/items';
 import { showOrders } from '../pages/orders';
 
 const domEvents = () => {
@@ -15,7 +15,7 @@ const domEvents = () => {
 
     // TODO: CLICK EVENT FOR DELETING A ORDER
     if (e.target.id.includes('delete-order-btn')) {
-    // eslint-disable-next-line no-alert
+      // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
         console.warn('CLICKED DELETE ORDER', e.target.id);
         console.warn(e.target.id.split('--'));
@@ -30,13 +30,10 @@ const domEvents = () => {
       console.warn('EDIT ORDER', e.target.id);
       console.warn(e.target.id.split('--'));
       const [, firebaseKey] = e.target.id.split('--');
-      getSingleOrder(firebaseKey).then((order) => (createOrderForm(order)));
+      getSingleOrder(firebaseKey).then((order) => createOrderForm(order));
     }
     if (e.target.id.includes('view-order-btn')) {
-      console.warn('VIEW YOUR ITEM', e.target.id);
-      console.warn(e.target.id.split('--'));
-      const [, firebaseKey] = e.target.id.split('--');
-      emptyItems(firebaseKey).then((item) => (showItems(item)));
+      getOrders().then(showOrders);
     }
   });
 };
