4e77ee066342e63bf60d74de5c48f752ec684508
Skeo <skeo051600@gmail.com>
2024-06-22 00:21:07 -0500

    Fix data and began edit on edit orders

--- pages/orders.js
+++ pages/orders.js
@@ -6,13 +6,13 @@ const emptyOrders = () => {
   renderToDom('#orders', domString);
 };
 
+// prettier-ignore
 const showOrders = (array) => {
   clearDom();
 
   let domString = '';
   array.forEach((order) => {
-    if (order.open === true) {
-      domString += `
+    domString += `
       <div class="card order">
         <div class="card-body">
           <h2 class="card-title card-order-name">${order.orderName}</h2>
@@ -26,22 +26,6 @@ const showOrders = (array) => {
         </div>
       </div>
       `;
-    } else {
-      domString += `
-      <div class="card order">
-        <div class="card-body">
-          <h2 class="card-title card-order-name">${order.orderName}</h2>
-          <p class="card-text card-order-status">Order Status: Closed</p>
-          <p class="card-text card-phone-number">Customer Phone Number: ${order.customerPhone}</p>
-          <p class="card-text card-email-address">Customer Email Address: ${order.customerEmail}</p>
-          <p class="card-text card-order-type">Order Type: ${order.orderType}</p>
-          <button id="view-order-btn--${order.firebaseKey}" class="btn btn-link">Details</button>
-          <button id="edit-order-btn--${order.firebaseKey}" class="btn btn-link">Edit</button>
-          <button id="delete-order-btn--${order.firebaseKey}" class="btn btn-link">Delete</button>
-        </div>
-      </div>
-      `;
-    }
   });
   renderToDom('#app', domString);
 };
